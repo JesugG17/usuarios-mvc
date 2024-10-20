8b673d1bdc498f26540a04fe4fa7560ee035cde5
andrew <andtr@protonmail.com>
2024-10-20 16:53:26 -0700

    fix: fix lock and setActive

--- src/main/java/com/mycompany/db/BaseDeDatos.java
+++ src/main/java/com/mycompany/db/BaseDeDatos.java
@@ -4,13 +4,18 @@ import com.mycompany.entities.Registro;
 import com.mycompany.entities.Usuario;
 
 public abstract class BaseDeDatos {
-  public abstract int registrarUsuario(Registro registro);
 
-  public abstract Usuario obtenerUsuarioPorCorreo(String correo);
+    public abstract int registrarUsuario(Registro registro);
 
-  public abstract int actualizarIntentos(String correo);
+    public abstract Usuario obtenerUsuarioPorCorreo(String correo);
 
-  public abstract int actualizarActivo(String correo);
+    public abstract int actualizarIntentos(String correo);
 
-  public abstract int actualizarFechaBloqueo(String correo);
+    public abstract int actualizarActivo(String correo);
+
+    public abstract int actualizarFechaBloqueo(String correo);
+
+    public abstract int reiniciarIntentos(String correo);
+
+    public abstract int reiniciarFechaBloqueo(String correo);
 }
