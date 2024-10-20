a6723734d14d6f77478b8ca5b959261bc8307192
andrew <andtr@protonmail.com>
2024-10-19 20:38:26 -0700

    feat: add registrarUsuario, validarIngreso in Modelo

--- src/main/java/com/mycompany/Modelo.java
+++ src/main/java/com/mycompany/Modelo.java
@@ -1,27 +1,72 @@
 package com.mycompany;
+
+import com.mycompany.PasswordEncryption;
 import com.mycompany.db.BaseDeDatos;
+import com.mycompany.db.BaseDeDatosImplementation;
 import com.mycompany.entities.Registro;
 import com.mycompany.entities.Usuario;
+
 public class Modelo {
+
     private BaseDeDatos bd;
-    public Modelo(BaseDeDatos bd) {
+
+    public Modelo(BaseDeDatosImplementation bd) {
         this.bd = bd;
     }
+
     public void validarIngreso(Usuario usuario) {
-        
+        if (
+            !PasswordEncryption.isPasswordMatch(
+                usuario.getNip(),
+                bd.obternerUsuarioPorCorreo(usuario).getNip()
+            ) ||
+            (!bd
+                    .obternerUsuarioPorCorreo(usuario)
+                    .getCorreo()
+                    .equals(usuario.getCorreo()))
+        ) {
+            System.out.println("Contraseña incorrecta o usuario incorrecto");
+            // actualizar intentos fallidos incrementar
+            return;
+        }
+
+        System.out.println("Bienvenido " + usuario.getCorreo());
     }
 
     public boolean registrarUsuario(Registro registro) {
-        if(!validaciones(registro))
-            return false;
+        if (!validaciones(registro)) return false;
+        registro.setPassword1(
+            PasswordEncryption.encryptPassword(registro.getPassword1())
+        );
         bd.registrarUsuario(registro);
         return true;
     }
 
     private boolean validaciones(Registro registro) {
+        if (
+            !registro
+                .getCorreo()
+                .matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
+        ) {
+            System.out.println("Correo no valido");
+            return false;
+        }
+
+        if (
+            registro.getPassword1().matches("^\\d{4}$") !=
+            registro.getPassword2().matches("^\\d{4}$")
+        ) {
+            System.out.println(
+                "Contraseña no valida, solo numeros de 4 digitos"
+            );
+            return false;
+        }
+        if (!registro.getPassword1().equals(registro.getPassword2())) {
+            System.out.println("Contraseñas no coinciden");
+            return false;
+        }
         return true;
     }
 
-    public void cerrarSeccion() {
-    }
+    public void cerrarSeccion() {}
 }
---
+++ src/main/java/com/mycompany/PasswordEncryption.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncryption {
+
+    private static BCryptPasswordEncoder passwordEncoder =
+        new BCryptPasswordEncoder();
+
+    public static String encryptPassword(String plainPassword) {
+        return passwordEncoder.encode(plainPassword);
+    }
+
+    public static boolean isPasswordMatch(
+        String plainPassword,
+        String hashedPassword
+    ) {
+        return passwordEncoder.matches(plainPassword, hashedPassword);
+    }
+}
