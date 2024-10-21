00f11003afa895fb88c94cce9370a9594d00bcae
Javier Eduardo <javiereduardo55xd@gmail.com>
2024-10-20 21:08:53 -0700

    agregando validaciones a la contraseña

--- src/main/java/com/mycompany/Modelo.java
+++ src/main/java/com/mycompany/Modelo.java
@@ -27,40 +27,33 @@ public class Modelo {
             return new Response(false, "Ya hay una sesión activa");
         }
 
-        if (
-            usuarioBD.getFecha_bloqueado() != null &&
-            usuarioBD.getFecha_bloqueado().after(new Date())
-        ) {
+        if (usuarioBD.getFecha_bloqueado() != null &&
+                usuarioBD.getFecha_bloqueado().after(new Date())) {
             Instant fechaActual = new Date().toInstant();
 
             long segundosRestantes = Duration.between(
-                fechaActual,
-                usuarioBD.getFecha_bloqueado().toInstant()
-            ).getSeconds();
-
+                    fechaActual,
+                    usuarioBD.getFecha_bloqueado().toInstant()).getSeconds();
 
             long minutosRestantes = segundosRestantes / 60;
             long segundos = segundosRestantes % 60;
             String tiempo;
 
             if (minutosRestantes >= 1) {
-              String unidadMinutos  = minutosRestantes == 1 ? " minuto" : " minutos";
-              tiempo = minutosRestantes + unidadMinutos;
+                String unidadMinutos = minutosRestantes == 1 ? " minuto" : " minutos";
+                tiempo = minutosRestantes + unidadMinutos;
             } else {
-              String unidadSegundos = segundos == 1 ? " segundo" : " segundos";
-              tiempo = segundos + unidadSegundos;
+                String unidadSegundos = segundos == 1 ? " segundo" : " segundos";
+                tiempo = segundos + unidadSegundos;
             }
 
             return new Response(
-                false,
-                "Usuario bloqueado, intente mas tarde en: " + tiempo
-            );
+                    false,
+                    "Usuario bloqueado, intente mas tarde en: " + tiempo);
         }
 
-        if (
-            usuarioBD.getFecha_bloqueado() != null &&
-            usuarioBD.getFecha_bloqueado().before(new Date())
-        ) {
+        if (usuarioBD.getFecha_bloqueado() != null &&
+                usuarioBD.getFecha_bloqueado().before(new Date())) {
             bd.reiniciarIntentos(usuarioBD.getCorreo());
             bd.reiniciarFechaBloqueo(usuarioBD.getCorreo());
             usuarioBD = bd.obtenerUsuarioPorCorreo(usuario.getCorreo());
@@ -69,9 +62,8 @@ public class Modelo {
         if (usuarioBD.getNum_intentos() >= 3) {
             bd.actualizarFechaBloqueo(usuario.getCorreo());
             return new Response(
-                false,
-                "Numero de intentos maximo alcanzado, se ha bloqueado su cuenta por 30m"
-            );
+                    false,
+                    "Numero de intentos maximo alcanzado, se ha bloqueado su cuenta por 30m");
         }
 
         if (!Encrypter.matchPasswords(usuario.getNip(), usuarioBD.getNip())) {
@@ -81,15 +73,15 @@ public class Modelo {
 
         bd.actualizarActivo(usuario.getCorreo());
         return new Response(
-            true,
-            "Inicio de sesión exitoso",
-            usuario.getCorreo()
-        );
+                true,
+                "Inicio de sesión exitoso",
+                usuario.getCorreo());
     }
 
     public Response registrarUsuario(Registro registro) {
         Response validaciones = validaciones(registro);
-        if (!validaciones.isValid()) return validaciones;
+        if (!validaciones.isValid())
+            return validaciones;
 
         Usuario usuarioBd = bd.obtenerUsuarioPorCorreo(registro.getCorreo());
 
@@ -103,11 +95,9 @@ public class Modelo {
     }
 
     private Response validaciones(Registro registro) {
-        if (
-            !registro
+        if (!registro
                 .getCorreo()
-                .matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
-        ) {
+                .matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
             return new Response(false, "Correo no valido");
         }
 
@@ -115,11 +105,25 @@ public class Modelo {
             return new Response(false, "Las contraseñas no coinciden");
         }
 
-        if (!registro.getPassword1().matches("^\\d{4}$")) {
+        if (registro.getPassword1().length() < 8) {
             return new Response(
-                false,
-                "Contraseña no valida, solo numeros de 4 digitos"
-            );
+                    false,
+                    "Contraseña no valida, tiene que tener más de 8 caracteres");
+        }
+        if (!registro.getPassword1().matches(".*[0-9].*")) {
+            return new Response(
+                    false,
+                    "Contraseña no valida, tiene que tener por lo menos un numero");
+        }
+        if (!registro.getPassword1().matches(".*[!\"#$%&/()=?¡].*")) {
+            return new Response(
+                    false,
+                    "Contraseña no valida, tiene que tener un caracter especial: !\"#$%&/()=?¡");
+        }
+        if (registro.getPassword1().matches(".*(123|321|1234|4321|abcd|qwerty|password|12345|54321|0000).*")) {
+            return new Response(
+                    false,
+                    "Contraseña no valida, no puede tener patrones familiares 123,1234,abcd,password etc.");
         }
         return new Response(true);
     }
